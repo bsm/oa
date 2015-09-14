a61bb2977c93f04ce0cb89da400ea57d079baed1
John Medforth <john.medforth@blacksquaremedia.com>
2015-09-14 16:16:22 +0100

    Splitting the routes.

--- lib/bsm_oa/routes.rb
+++ lib/bsm_oa/routes.rb
@@ -8,6 +8,11 @@ module BsmOa
     module Helper
 
       def mount_bsm_oa
+        mount_bsm_oa_admin
+        mount_bsm_oa_callbacks
+      end
+
+      def mount_bsm_oa_admin
         get 'me(.:format)', to: 'bsm_oa/accounts#show', as: :bsm_oa_me
         resources :roles, controller: 'bsm_oa/roles', as: :bsm_oa_roles do
           resources :authorizations, controller: 'bsm_oa/authorizations', as: :bsm_oa_authorizations, shallow: true do
@@ -16,6 +21,13 @@ module BsmOa
         end
         use_doorkeeper do
           controllers applications: 'bsm_oa/applications'
+          skip_controllers :authorized_applications, :authorizations, :tokens
+        end
+      end
+
+      def mount_bsm_oa_callbacks
+        use_doorkeeper do
+          skip_controllers :applications, :authorized_applications
         end
       end
 
